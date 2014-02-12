ac843c00725749df1adfc89b15aea920b2fdf053
p3miki <p3miki@hotmail.com>
2014-02-12 16:20:23 -0500

    Ajouter classe: -AnalyseurCircuit -BD -ControleurCircuit

---
+++ ProjetIntegrateur/src/projetintegrateur/AnalyseurCircuit.java
@@ -0,0 +1,52 @@
+package projetintegrateur;
+
+public class AnalyseurCircuit {
+    
+    public AnalyseurCircuit() {
+        
+    }
+    
+    public void analyserCircuit() {
+        
+    }
+    
+    /*
+     ***********************************
+     * Ajouter les infos a retourner   *
+     ***********************************
+     */
+    
+    public void getValeurComposante(int noComp) {
+        
+    }
+    
+    /*
+     ******************************************
+     * Changer le return pour un vrai circuit *
+     ******************************************
+     */
+    
+    public Circuit decoderCircuit() {
+        return new Circuit();
+    }
+    
+    /*
+     ***********************************
+     * Ajouter les infos a retourner   *
+     ***********************************
+     */
+    
+    public void creerCircuit(Circuit c) {
+        
+    }
+    
+    /*
+     ***********************************
+     * Ajouter le XML a retourner      *
+     ***********************************
+     */
+    
+    public void encoderCircuit() {
+        
+    }
+}
---
+++ ProjetIntegrateur/src/projetintegrateur/ControleurCircuit.java
@@ -0,0 +1,24 @@
+package projetintegrateur;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class ControleurCircuit {
+    ArrayList<Circuit> circuitsOuverts = new ArrayList<>();
+    
+    public ControleurCircuit(){
+        
+    }
+    
+    public void nouveauCircuit(){
+        
+    }
+    
+    public boolean ouvrirCircuit(Path emplacement) {
+        return true;
+    }
+    
+    public void enregistreCircuit(){
+        
+    }
+}
