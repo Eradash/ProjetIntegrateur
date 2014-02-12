05422745a97f552c613f02b929222ca703fee4b1
Eradash <poirier.20.100@hotmail.com>
2014-02-12 15:47:38 -0500

    Simplification du main

--- ProjetIntegrateur/src/projetintegrateur/ProjetIntegrateur.java
+++ ProjetIntegrateur/src/projetintegrateur/ProjetIntegrateur.java
@@ -1,26 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package projetintegrateur;
 
-/**
- *
- * @author Eradash
- */
 public class ProjetIntegrateur {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        // Test de fusion de code
-        
-        //GROS TEST DE LA MORT !!!
     }
     
 }
