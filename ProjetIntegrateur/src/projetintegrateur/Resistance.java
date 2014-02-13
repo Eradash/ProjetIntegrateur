a886318f674e43a3cc4123e9b895f2b0c377229e
Eradash <poirier.20.100@hotmail.com>
2014-02-12 19:43:06 -0500

    Classe résistance
    Conforme au plan, reste juste à coder les fonctions comme du monde...

--- ProjetIntegrateur/src/projetintegrateur/Resistance.java
+++ ProjetIntegrateur/src/projetintegrateur/Resistance.java
@@ -3,6 +3,15 @@ package projetintegrateur;
 public class Resistance implements Composante{
 
     int numero;
+    int ohm;
+    
+    public int getOhm(){
+        return ohm;
+    }
+    
+    public boolean calculExplosion(int ampere){
+        return true;
+    }
     
     @Override
     public int getNumero() {
