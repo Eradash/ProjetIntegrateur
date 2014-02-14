685db14d1592ca3e1cc3bf2bd2b292e9ecb97063
Eradash <poirier.20.100@hotmail.com>
2014-02-14 15:41:42 -0500

    Séparation des listes de composantes
    Plus simple pour faire les calculs…

--- ProjetIntegrateur/src/projetintegrateur/Branche.java
+++ ProjetIntegrateur/src/projetintegrateur/Branche.java
@@ -1,11 +1,8 @@
 package projetintegrateur;
 
-import java.util.ArrayList;
-
 public abstract class Branche implements Composante{
     
     int numero;
-    ArrayList<ArrayList<Composante>> listeDeComposantes;
     
     @Override
     public int getNumero(){
