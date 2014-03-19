120c218bd9f7e0f926e901dd193f5ef4ab339bf7
Eradash <poirier.20.100@hotmail.com>
2014-03-19 15:14:07 -0400

    Fonction recherche

--- ProjetIntegrateur/src/gestion/ControleurCircuit.java
+++ ProjetIntegrateur/src/gestion/ControleurCircuit.java
@@ -3,12 +3,15 @@ package gestion;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import logiqueCircuit.Circuit;
+import logiqueCircuit.Composante;
 
 public class ControleurCircuit {
     
     private final static GestionXML xml = GestionXML.getInstance();
     ArrayList<Circuit> circuitsOuverts = new ArrayList<>();
     GestionnaireID gestID = GestionnaireID.getInstance();
+    
+    Circuit c = new Circuit();
     
     public ControleurCircuit(){
         
@@ -31,5 +34,9 @@ public class ControleurCircuit {
     
     public void enregistrerCircuit(Circuit c) throws Exception{
         xml.encoder(c);
+    }
+    
+    public void ajouterComposante(Composante c, int emplacement){
+        this.c.ajouterComposante(c, emplacement);
     }
 }
