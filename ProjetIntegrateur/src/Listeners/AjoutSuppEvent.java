09b6aeb11a1998274a7fcbe1988da504c458e326
Eradash <poirier.20.100@hotmail.com>
2014-03-21 15:01:10 -0400

    Listeners dans ControlleurCircuit

--- ProjetIntegrateur/src/Listeners/AjoutSuppEvent.java
+++ ProjetIntegrateur/src/Listeners/AjoutSuppEvent.java
@@ -2,4 +2,13 @@ package Listeners;
 
 public class AjoutSuppEvent{
     
+    private String type;
+    
+    public AjoutSuppEvent(String type){
+        this.type = type;
+    }
+    
+    public String getType(){
+        return type;
+    }
 }
--- ProjetIntegrateur/src/gestion/ControleurCircuit.java
+++ ProjetIntegrateur/src/gestion/ControleurCircuit.java
@@ -10,13 +10,15 @@ import logiqueCircuit.Composante;
 public class ControleurCircuit implements ComposanteObservable{
     
     private final static GestionXML xml = GestionXML.getInstance();
-    ArrayList<Circuit> circuitsOuverts = new ArrayList<>();
+    ArrayList<Circuit> circuitsOuverts;
     GestionnaireID gestID = GestionnaireID.getInstance();
+    ArrayList<ComposanteListener> listeListeners;
     
     Circuit c = new Circuit();
     
     public ControleurCircuit(){
-        
+        circuitsOuverts = new ArrayList<>();
+        listeListeners = new ArrayList<>();
     }
     
     public void nouveauCircuit(){
@@ -44,16 +46,25 @@ public class ControleurCircuit implements ComposanteObservable{
 
     @Override
     public void ajouterListener(ComposanteListener listener) {
-
+        listeListeners.add(listener);
     }
 
     @Override
     public void supprimerListener(ComposanteListener listener) {
-
+        listeListeners.remove(listener);
     }
 
     @Override
     public void notifierAjoutSuppComposante(AjoutSuppEvent event) {
-
+        switch (event.getType()) {
+            case "ajout":
+                for(ComposanteListener listener : listeListeners){
+                    listener.composanteAjout(event);
+                }   break;
+            case "suppression":
+                for(ComposanteListener listener : listeListeners){
+                    listener.composanteSupp(event);
+                }   break;
+        }
     }
 }
