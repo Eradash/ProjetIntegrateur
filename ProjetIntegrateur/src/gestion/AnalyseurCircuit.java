458b3b56154242b349975b18c3aa830efca4f10c
Eradash <poirier.20.100@hotmail.com>
2014-04-11 15:18:25 -0400

    Ajout des valeurs à la BD

--- ProjetIntegrateur/src/gestion/AnalyseurCircuit.java
+++ ProjetIntegrateur/src/gestion/AnalyseurCircuit.java
@@ -10,39 +10,53 @@ public class AnalyseurCircuit {
         double voltage = c.getVoltage();
         double ampere = c.getAmpere();
         
-        calcul(ampere, voltage, c);
+        calcul(ampere, voltage, c, -1);
     }
     
-    private void calcul(double amp, double volt, Composante b) {
+    private void calcul(double amp, double volt, Composante b, int ID_Parent) {
+        int ID = b.getNumero();
         double resistance = b.getResistanceEquivalente();
         double ampere = amp;
         double voltage = volt;
+        
+        BD bd = BD.getInstance();
+        
+        bd.SetComposante(ID, "Parent", ID_Parent);
+        
         int cas = 0;
 
         if (ampere != -1 || voltage != -1) {
             if (ampere == -1 && voltage != -1) {
                 ampere = voltage / resistance;
-                BD.getInstance().SetComposante(b.getNumero(), "Ampere", ampere);
+                bd.SetComposante(ID, "Ampere", ampere);
+                bd.SetComposante(ID, "Voltage", voltage);
             } else if (voltage == -1 && ampere != -1) {
                 cas += 10;
                 voltage = resistance * ampere;
-                BD.getInstance().SetComposante(b.getNumero(), "Voltage", voltage);
+                bd.SetComposante(ID, "Voltage", voltage);
+                bd.SetComposante(ID, "Ampere", ampere);
             }
         }
 
-        if (b.getType() == Type.SERIE || b.getType() == Type.CIRCUIT) { 
+        if (b.getType() == Type.SERIE){
+            cas +=1;
+            bd.SetComposante(ID, "Type", 1);
+        } else if(b.getType() == Type.CIRCUIT){
             cas += 1;
+            bd.SetComposante(ID, "Type", 3);
         } else if (b.getType() == Type.PARALLELE) {
             cas += 2;
+            bd.SetComposante(ID, "Type", 2);
         } else {
             cas = 0;
+            bd.SetComposante(ID, "Type", 4);
         }
         
         switch (cas) {
             case 1:
                 for (Composante c : b.getComposantes()) {
                     voltage = c.getResistanceEquivalente() * ampere;
-                    calcul(ampere, voltage, c);
+                    calcul(ampere, voltage, c, ID);
                 }
                 break;
 
@@ -50,7 +64,7 @@ public class AnalyseurCircuit {
             case 11:
             case 12:
                 for (Composante c : b.getComposantes()) {
-                    calcul(ampere, voltage, c);
+                    calcul(ampere, voltage, c, ID);
                 }
                 break;
         }
