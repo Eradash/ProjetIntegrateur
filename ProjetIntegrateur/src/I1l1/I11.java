4f6f72a4767bf065cf46ab6ee1f99c9f7a881de9
Eradash <poirier.20.100@hotmail.com>
2014-05-12 15:02:19 -0400

    Revert f33471c..55dc11a
    This rolls back to commit f33471c7417cba9ac69a0a5319a90dd35f141e96.

--- ProjetIntegrateur/src/I1l1/I11.java
+++ ProjetIntegrateur/src/I1l1/I11.java
@@ -1,17 +1,15 @@
 package I1l1;
 
 import I11l.IIll;
-import I1ll.I1i;
 import I1ll.Ii;
+import I1ll.I1i;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import l.I1;
 import l.Il;
-import l.Il1;
 import l.Ill;
-
-
+import l.I1;
+import l.Il1;
 
 public class I11 {
 
@@ -75,7 +73,7 @@ public class I11 {
                     if (ll(new Il1[]{Il1.ll, Il1.i}, il)) {
                         String Il = l("l", "l");
                         if ((li = ll(Il)) > 0) {
-                            ll.il(new l.Ill(li), il);
+                            ll.il(new Ill(li), il);
                         }
                     }
                     break;
--- ProjetIntegrateur/src/I1ll/Ii.java
+++ ProjetIntegrateur/src/I1ll/Ii.java
@@ -11,36 +11,87 @@ public class Ii {
 
     public void il(I1l i) {
         if (i != null) {
-            double l = i
-                    .il();
+            double l = i.il();
             il(-1, l, i, -1);
         }
     }
 
     private void il(double i, double il, I l, int li) {
-        int ll=l.I();double Il=l.Il();double lI=i;double
-        l1=il;Iil I1=Iil.ll();I1.il(ll,"Il",li);I1.il(ll
-        ,"l",il(Il));int II=0;if((lI!=-1||l1!=-1)&&(lI!=
-        -1||l1==-1))if(l1==-1&&lI!=-1){II+=10;l1=Il*lI;}
-        else{lI=l1/Il;}I1.il(ll,"Ii",il(lI));I1.il(ll,""
-        +"iI",il(l1));if(l.II()!=Il1.i){if(l.II()!=Il1.ll
-        ){if(l.II()!=Il1.l){if(l.II()!= Il1.li){II+=1;I1
-        .il(ll,"I1",1);}else{II=0;I1.il(ll,"I1",4);}}else
-        {II+=2;I1.il(ll,"I1",2);}}else{II+=1;I1.il(ll,""
-        +"I1",3);}}if(l.II()==Il1.li){double lIl=lI*l1;I1
-        .il(ll,"I1l",il(lIl));Ill IlI=(Ill)l;if(lIl>0.25
-        ){IlI.il(true);}else{IlI.il(false);}}if(II==1){}
-        else if((II!=2&&II!=11)&&II!=12){for(I c:l.lI())
-        {l1=c.Il()*lI;il(lI,l1,c,ll);}}else{for(I c:l.lI
-        ()){il(-1,l1,c,ll);}}
+        int ll = l.I();
+        double Il = l.Il();
+        double lI = i;
+        double l1 = il;
+
+        Iil I1 = Iil.ll();
+
+        I1.il(ll, "Parent", li);
+        I1.il(ll, "Resistance", il(Il));
+
+        int II = 0;
+
+        if (lI != -1 || l1 != -1) {
+            if (lI == -1 && l1 != -1) {
+                lI = l1 / Il;
+            } else if (l1 == -1 && lI != -1) {
+                II += 10;
+                l1 = Il * lI;
+            }
+        }
+
+        I1.il(ll, "Ampere", il(lI));
+        I1.il(ll, "Voltage", il(l1));
+
+        switch (l.II()) {
+            case i:
+                II += 1;
+                I1.il(ll, "Type", 1);
+                break;
+            case ll:
+                II += 1;
+                I1.il(ll, "Type", 3);
+                break;
+            case l:
+                II += 2;
+                I1.il(ll, "Type", 2);
+                break;
+            case li:
+                II = 0;
+                I1.il(ll, "Type", 4);
+                break;
+        }
+
+        if (l.II() == Il1.li) {
+            double lIl = lI * l1;
+            I1.il(ll, "Watt", il(lIl));
+            Ill IlI = (Ill) l;
+            if (lIl > 0.25) {
+                IlI.il(true);
+            } else {
+                IlI.il(false);
+            }
+        }
+
+        switch (II) {
+            case 1:
+                for (I c : l.lI()) {
+                    l1 = c.Il() * lI;
+                    il(lI, l1, c, ll);
+                }
+                break;
+
+            case 2:
+            case 11:
+            case 12:
+                for (I c : l.lI()) {
+                    il(-1, l1, c, ll);
+                }
+                break;
+        }
     }
 
     private double il(double l) {
-        if (l != Double.NEGATIVE_INFINITY
-                && l != Double.POSITIVE_INFINITY
-                && l != Double.NaN) {
-            double ll = l
-                    * Math.pow(10, l);
+        if(l != Double.NEGATIVE_INFINITY && l != Double.POSITIVE_INFINITY && l != Double.NaN){
+            double ll = l * Math.pow(10, l);
             ll = Math.round(ll);
             ll = ll / Math.pow(10, l);
             return ll;
